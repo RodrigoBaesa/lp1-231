90b671e0874d7cb4f32ced721d701128692a572e
Rodrigo <rodrigobaesa@gmail.com>
2023-03-14 17:56:15 -0300

    Introdução da pasta de exercicios

---
+++ src/exercicios/Ex01.java
@@ -0,0 +1,8 @@
+package exercicios;
+
+public class Ex01 {
+    //Escreva um programa em Java que solicita ao usuário um número inteiro e apresenta seu antecessor e sucessor.
+    public static void main(String[] args) {
+        
+    }
+}
