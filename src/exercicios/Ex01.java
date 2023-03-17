3fcfa7016a164df5e06e43aad58671daa03ad31d
Rodrigo <rodrigobaesa@gmail.com>
2023-03-16 22:50:55 -0300

    Introdução do exercicio01

--- src/exercicios/Ex01.java
+++
@@ -1,8 +0,0 @@
-package exercicios;
-
-public class Ex01 {
-    //Escreva um programa em Java que solicita ao usuário um número inteiro e apresenta seu antecessor e sucessor.
-    public static void main(String[] args) {
-        
-    }
-}
