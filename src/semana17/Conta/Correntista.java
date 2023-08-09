ade02a34703836ed23d5e54ed7b9beae0ba9f1e4
Rodrigo <rodrigobaesa@gmail.com>
2023-08-08 23:37:28 -0300

    Semana 17 exercicios

--- src/semana17/Conta/Correntista.java
+++ src/semana17/Conta/Correntista.java
@@ -1,10 +1,17 @@
 package semana17.Conta;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Correntista {
 
     private int codigo;
     private String nome;
-    private List<Conta> contas;
+    private List<Conta> Conta;
+
+    public Correntista(int codigo, String nome) {
+        this.codigo = codigo;
+        this.nome = nome;
+        Conta = new ArrayList<>();
+    }
 }
